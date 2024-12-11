995ba527228b476cbb51a162a6b4447f83e56114
Lizok636 <egorovaelizaveta041@gmail.com>
2024-12-11 23:28:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
+count=5
 with open("cars.json", 'r', encoding='utf-8') as file:
  inf = json.load(file)
- commands = [
+commands = [
     "Вывести все записи",
     "Вывести запись по полю",
     "Добавить запись",
@@ -17,6 +18,53 @@ while True:
     5.Выйти из программы""" )
   num=int(input("Ваше значение: "))
   if num==1:
+    for car in inf:
+      if car["is_petrol"] == True:
+        pet="да"
+      else:
+        pet="нет"
+      print(f"""
+      Номер записи: {car["id"]},
+      Название модели: {car["name"]},
+      Название производителя: {car["manufacturer"]},
+      Заправляется бензином: {pet},
+      Объем бака: {car["tank_volume"]}
+      """)
+  elif num==2:
+     id=input("Введите номер записи машины: ")
+     while not id.isdigit():
+        print("Неккоректное значение. Попробуйте еще раз :)")
+        id = input("Введите номер записи машины : ")
+     for car in inf:
+        if id == car.get("id"):
+          if car["is_petrol"] == True:
+               pet="да"
+          else:
+               pet="нет"
+          print(f"""
+          Номер записи: {car["id"]},
+          Название модели: {car["name"]},
+          Название производителя: {car["manufacturer"]},
+          Заправляется бензином: {pet},
+          Объем бака: {car["tank_volume"]}
+          """)
+          break
+        else:
+          print("Запись не найдена")
+  elif num==3:
+     proverka=False
+     new_id=count+1
+     new_name = input("Введите название модели: ")
+     new_manufacturer = input("Введите производителя: ")
+     new_is_petrol = input("Машина заправляется бензином (да/нет): ").lower()
+     if new_is_petrol=="да" or new_is_petrol=="нет":
+      new_is_petrol = new_is_petrol.lower().strip()
+     else:
+        while proverka==False:
+           new_is_petrol = input("Машина заправляется бензином (да/нет): ").lower()
+           if new_is_petrol=="да" or new_is_petrol=="нет":
+            new_is_petrol = new_is_petrol.lower().strip()
+            proverka==True
     
 
  
